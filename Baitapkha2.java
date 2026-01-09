75da7163e6702514c881e6353cb9070e15d0d91e
tranleanhkhoa041221-cpu <tranleanhkhoa041221@gmail.com>
2026-01-09 20:42:27 +0700

    uupload btvnsession2_khá2

---
+++ Baitapkha2.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Baitapkha2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số tháng : ");
+        int month = Integer.parseInt(sc.nextLine());
+        switch (month) {
+            case 1: {
+                System.out.println("Tháng 1 có 31 ngày");
+                break;
+            }
+            case 2: {
+                System.out.println("Tháng 2 có 28 hoặc 29 ngày");
+                break;
+            }
+            case 3: {
+                System.out.println("Tháng 3 có 31 ngày");
+            }
+            case 4: {
+                System.out.println("Tháng 4 có 30 ngày");
+            }
+            case 5: {
+                System.out.println("Tháng 5 có 31 ngày");
+            }
+            case 6: {
+                System.out.println("Tháng 6 có 30 ngày");
+            }
+            case 7: {
+                System.out.println("Tháng 7 có 31 ngày");
+            }
+            case 8: {
+                System.out.println("Tháng 8 có 31 ngày");
+            }
+            case 9: {
+                System.out.println("Tháng 9 có 30 ngày");
+            }
+            case 10: {
+                System.out.println("Tháng 10 có 31 ngày");
+            }
+            case 11: {
+                System.out.println("Tháng 11 có 30 ngày");
+            }
+            case 12: {
+                System.out.println("Tháng 12 có 31 ngày");
+            }
+            default:
+                System.out.println("Tháng không hợp lệ");
+
+        }
+
+    }
+}
